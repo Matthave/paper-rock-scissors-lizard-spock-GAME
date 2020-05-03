151af77f1ed1f9b5e6c49eb32bd652822fbc8936
Matt <matthave@gmail.com>
2020-05-03 04:33:42 +0200

    Style changes

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -4,8 +4,10 @@ import rulesImg from '../images/image-rules-bonus.svg'
 function Rules(props) {
   return (
     <div className='rules'>
-      <div className="rules__escape" onClick={props.showRules}></div>
-      <img className='rules__img' src={rulesImg} alt="" />
+      <div className="rules__wrap">
+        <div className="rules__escape" onClick={props.showRules}></div>
+        <img className='rules__img' src={rulesImg} alt="" />
+      </div>
     </div>
   )
 }
